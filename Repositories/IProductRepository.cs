a985ef9da496b6a358a99fe8a41ec90d6f62d9a6
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-09-16 16:58:11 +0700

    feat: Create ProductRepository.

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using SoccerShoesShop.Models;
+
+namespace SoccerShoesShop.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> findAllAsync();
+        Task<Product> findByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
