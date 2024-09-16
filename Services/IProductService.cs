63fade5f7e3665ce07f85c17102e8fe040f38d6f
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-09-16 16:58:50 +0700

    feat: Create ProductService.

---
+++ Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using SoccerShoesShop.Models;
+
+namespace SoccerShoesShop.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task AddProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+        Task UpdateProductAsync(Product product);
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using SoccerShoesShop.Common;
+using SoccerShoesShop.Models;
+using SoccerShoesShop.Repositories;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SoccerShoesShop.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger _logger;
+
+        public ProductService(IProductRepository productRepository, ILogger logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            var newProduct = new Product
+            {
+                ProductId = IdGenerator.GeneratedIdBasedOnTime(),
+                CategoryId = 1,
+                UserId = 1,
+                Name = product.Name,
+                Description = product.Description,
+                Discount = product.Discount,
+                Image = product.Image,
+                Quantity = product.Quantity,
+                SellingPrice = product.SellingPrice
+            };
+            await _productRepository.AddAsync(newProduct);
+            _logger.LogInformation("Save Product successfully");
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+            _logger.LogInformation($"Deleted {id} successfully");
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _productRepository.findAllAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _productRepository.findByIdAsync(id);
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            var productExisting = await _productRepository.findByIdAsync(product.ProductId);
+            if (productExisting == null) throw new ArgumentNullException("ProductExisting is null");
+            productExisting.CategoryId = product.CategoryId;
+            productExisting.UserId = product.UserId;
+            productExisting.Name = product.Name;
+            productExisting.Description = product.Description;
+            productExisting.Discount = product.Discount;
+            productExisting.Image = product.Image;
+            productExisting.Quantity = product.Quantity;
+            productExisting.SellingPrice = product.SellingPrice;
+        }
+    }
+}
