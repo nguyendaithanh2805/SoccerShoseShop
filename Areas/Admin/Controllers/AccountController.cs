275b09c9bbaccf34272bd6387613876b74bd43e3
nguyenvanson1406 <vipanhson1234@gmail.com>
2024-09-21 22:06:02 +0700

    feat: Add Index and Delete methods for Account.

---
+++ Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerShoesShop.Services;
+
+namespace SoccerShoesShop.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class AccountController : Controller
+	{
+		private readonly IAccountService _accountService;
+
+		public AccountController(IAccountService accountService)
+		{
+			_accountService = accountService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var accounts = await _accountService.GetAllAccountAsync();
+			return View(accounts);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Delete(int id)
+		{
+			await _accountService.DeleteAccountAsync(id);
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
--- obj/Debug/net8.0/SoccerShoesShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoccerShoesShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoccerShoesShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02687fe68b3f7af916c6316733bbddfee44aef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db35e0035883317c9873089f7b9887a16c691da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoccerShoesShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoccerShoesShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
